6dbd7f5d4a3684ae4e9a3b16354fb65898ff2d42
tommy88888 <45169876+tommy88888@users.noreply.github.com>
2023-08-31 20:51:02 +0700

    Update webpack.prod.js

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,6 +4,7 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
+console.log('domain', domain)
 
 const prodConfig = {
   mode: 'production',
